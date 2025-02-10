d8646519953239e0f56f3cb1c556bc14eb01c93b
Joey <39647@ma-web.nl>
2025-02-10 15:12:35 +0100

    tutorial finished

---
+++ fireflyArcade/Assets/scripts/LaunchCube.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchCube : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    public float m_Thrust = 20f;
+
+    void Start()
+    {
+        Console.WriteLine("milk me");
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_Rigidbody.AddForce(transform.up * m_Thrust);
+        }
+    }
+}
